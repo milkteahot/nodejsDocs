d4997606f2beece04b06eae6632bf70b49ac705e
milkteahot <greenegoode11@gmail.com>
2019-12-20 23:26:38 +0900

    add previous

---
+++ DOIT/server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var server = http.createServer();
+
+var port = 3000;
+server.listen(port, function(){
+    console.log("서버시작: %d", port);
+});
+
+server.on('connection', function(socket){
+    var addr = socket.address();
+    console.log('클라이언트가 접속: %s, %d', addr.address, addr.port);
+});
+
+server.on('request', function(req,res){
+    console.log('클라이언트 요청 들어왔다');
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+    res.write("<!doctype html>");
+    res.write("<html>");
+    res.write("<h1> hi </h1>");
+    res.write("</html>");
+    res.end();
+});
+
+server.on('close', function(){
+    console.log('서버 종료');
+});
