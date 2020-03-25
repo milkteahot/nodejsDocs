369b4990042042d5d3d91ad5c6c23d95bf1d3fb6
milkteahot <greenegoode11@gmail.com>
2020-03-25 16:40:38 +0900

    assignment1.0

--- routes.js
+++ routes.js
@@ -6,10 +6,38 @@ const requestHandler = (req, res) => {
     if(url === '/') {
         res.write('<html>');
         res.write('<head><title>Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">submit</button></body>')
+        res.write('<body><h1>Hello! Nice to meet you.</h1>');
+        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">submit</button></body>');
         res.write('</html>');
         return res.end();
     }
+    if(url === '/users') {
+        // fs.writeFileSync('message.txt', 'Dummy');
+        res.write('<html>');
+        res.write('<head><title>Message</title></head>');
+        res.write('<body><h1>Hello! Nice to meet you.</h1>');
+        res.write('<ul><li>Kelly</li><li>Mark</li><li>Lauren</li><li>Andrew</li></ul>')
+        res.write('</body></html>');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    if(url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('</html>');
+    res.end();
 }
 
 module.exports = requestHandler;
