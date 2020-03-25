95ba8d8b6c170ce66ef51601884c9aeaaa0e78c9
milkteahot <greenegoode11@gmail.com>
2020-03-25 16:44:45 +0900

    assignment2.0

--- routes.js
+++ routes.js
@@ -7,19 +7,17 @@ const requestHandler = (req, res) => {
         res.write('<html>');
         res.write('<head><title>Message</title></head>');
         res.write('<body><h1>Hello! Nice to meet you.</h1>');
-        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">submit</button></body>');
+        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">submit</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     if(url === '/users') {
-        // fs.writeFileSync('message.txt', 'Dummy');
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Message</title></head>');
         res.write('<body><h1>Hello! Nice to meet you.</h1>');
         res.write('<ul><li>Kelly</li><li>Mark</li><li>Lauren</li><li>Andrew</li></ul>')
         res.write('</body></html>');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
         return res.end();
     }
     if(url === '/create-user' && method === 'POST') {
@@ -30,14 +28,12 @@ const requestHandler = (req, res) => {
         });
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
+            console.log(parsedBody.split('=')[1]);
         });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head></head>');
-    res.write('</html>');
-    res.end();
 }
 
 module.exports = requestHandler;
