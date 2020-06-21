5edda7106cbaffbf9a225dd8500a50cc29cda225
milkteahot <greenegoode11@gmail.com>
2020-06-21 15:43:02 +0900

    Add homepage

--- egoing/main.js
+++ egoing/main.js
@@ -4,37 +4,67 @@ var url = require("url");
 
 var app = http.createServer(function(request, response) {
   var _url = request.url;
+  
   var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
   var title = queryData.id;
-  if (_url == "/") {
-    title = 'Welcome';
+//   console.log(url.parse(_url, true));
+
+  if(pathname === '/') {
+      if(queryData.id === undefined) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var template = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>WEB1-${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=Javascript">Javascript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      }
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
+            var template = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>WEB1-${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=Javascript">Javascript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+        })
+        
+  } else {
+    response.writeHead(404);
+    response.end('Not Found');
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description){
-      var template = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <title>WEB1-${title}</title>
-          <meta charset="utf-8">
-      </head>
-      <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=Javascript">Javascript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-      </body>
-      </html>
-      `;
-    response.end(template);
-  });
+  
   
 });
 
