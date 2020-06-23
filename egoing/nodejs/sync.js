616c3754f1d8fff817567a277e26a765a708bdfd
milkteahot <greenegoode11@gmail.com>
2020-06-23 14:01:52 +0900

    Add Sync Async

---
+++ egoing/nodejs/sync.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+/*
+console.log('A');
+var result = fs.readFile('sample.txt', 'utf8');
+console.log(result);
+console.log('C');
+*/
+
+//비동기는 3번째 인자에 콜백이 있어야 함.
+console.log('A');
+fs.readFile('sample.txt', 'utf8', function(err, result){
+    console.log(result);
+});
+console.log('C');
+// A -> C -> B
